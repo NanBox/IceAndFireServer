7774fe81b5a156222b9664ccfcf70c0d5687c229
LinNanQuan <49220824@qq.com>
2017-01-15 16:02:55 +0800

    增加猫姨的跳转

--- skip.js
+++ skip.js
@@ -1,3 +1,3 @@
 ﻿function CatelynTully(){
-		javascript:Android.goDetail('凯特琳·徒利','/person/Tully/Catelyn_Tully/Catelyn_Tully.png','/1/Tully/Catelyn_Tully/Catelyn_Tully.html');
+		javascript:Android.goDetail('凯特琳·徒利','/person/Tully/Catelyn_Tully/Catelyn_Tully.png','/person/Tully/Catelyn_Tully/Catelyn_Tully.html');
 }
